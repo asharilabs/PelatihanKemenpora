ab6b2c5f6519e5975aec074dcd40c6e8df7d3c10
Galih Ashari R <asharilabs@gmail.com>
2021-05-05 15:59:06 +0700

    latihan2

---
+++ latihan2.py
@@ -0,0 +1,19 @@
+# latihan di hari yang kedua
+# buat suatu aplikasi sederhana di python
+# jika saya memasukkan 1 --> 12 maka akan 
+# menampilkan nama bulannya
+
+# contoh: input index bulan = 12
+# maka akan print = Desember
+
+# definisi nama-nama bulan
+namabulan = ['januari', 'februari', 'maret', 'april', 'mei', 'juni', 
+            'juli','agustus', 'september', 'oktober', 'november', 'desember']
+# input index
+indexBul = int(input('masukkan index bulannya: '))
+
+# olah data bulan input
+if indexBul > 0 and indexBul < 13:
+    print ('yaitu bulan %s' %namabulan[indexBul - 1])
+else:
+    print('dont know')
