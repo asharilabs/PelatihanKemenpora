8049847b4c70800b78ef04a138c100fd6f0d24c7
Galih Ashari R <asharilabs@gmail.com>
2021-05-05 15:33:13 +0700

    nambahin isintance

--- harikedua.py
+++ harikedua.py
@@ -1,16 +1,22 @@
-bilangan = input('input bilangan: ')
+# bilangan = input('input bilangan: ')
 
-for i in range(int(bilangan) + 1):
-    if i != 0:
-        print('Hitung %d' %i)
+# for i in range(int(bilangan) + 1):
+#     if i != 0:
+#         print('Hitung %d' %i)
 
-x = 3
-y = 3
-a = [1,2,3]
-b = [1,2,3]
-print( x is y)
+# x = 3
+# y = 3
+# a = [1,2,3]
+# b = [1,2,3]
+# print( x is y)
 
-print(x is x)
-print([1,2,3] == [1,2,3])
+# print(x is x)
+# print([1,2,3] == [1,2,3])
 
-print ('123 ->' + (a is b))
+# print ('123 ->' + (a is b))
+
+lst = [1,2,3,4,5,['a','b','c','d']]
+for x in lst:
+    if isinstance(x, list):
+        for y in x:
+            print(y)
